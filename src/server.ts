a904093b8cdf18732b8017aba202d2bb33c80e19
Walison Teodoro <64755624+Wtheodoro@users.noreply.github.com>
2020-10-16 17:33:23 -0300

    Second day done.

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,18 @@
-import express, { response } from 'express'
+import express from 'express'
+import 'express-async-errors'
 import './database/connection'
 import routes from './routes'
+import path from 'path'
+import errorHandler from './errors/handler'
+import cors from 'cors'
 
 const app = express()
+
+app.use(cors())
 app.use(express.json()) // serve pro express entender json
 app.use(routes)
-
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
+app.use(errorHandler)
 // rota = conjunto
 // recurso = usuario
 // métodos HTTP
